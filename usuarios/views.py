0d0f5c87fb4945f1da470f8e6a68301acbf244a0
Raglig <ddavi.barbosaalves02@gmail.com>
2026-05-25 07:48:26 -0300

    Configura app Usuarios e rotas de login e registro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def login_view(request):
+    """
+    Renderiza a view de login provisória.
+    """
+    return HttpResponse("Página de Login (Em Construção)")
+
+
+def registro_view(request):
+    """
+    Renderiza a view de registro provisória.
+    """
+    return HttpResponse("Página de Registro (Em Construção)")
+
