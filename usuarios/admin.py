6c25d8959af225c20b77e0ee8dcb7fa483368e9d
RespOn1 <arthurezbar@gmail.com>
2026-05-26 01:19:14 -0300

    feat: Adicionanando as configurações de admin dos usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,1 +1,13 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import PerfilUsuario
+
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    # Mostra essas colunas na tabela principal
+    list_display = ("usuario", "keycloak_id")
+
+    # AQUI ESTÁ O SEGREDO: Trava o campo para ninguém fazer besteira!
+    readonly_fields = ("keycloak_id", "usuario")
